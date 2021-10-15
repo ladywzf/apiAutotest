46da3f735bd252081ff81909f0ef768e8416cc4e
ladywzf <2975724990@qq.com>
2021-10-15 13:06:29 +0800

    commit

--- bin/test_run.py
+++ bin/test_run.py
@@ -9,14 +9,14 @@ if __name__ == '__main__':
     testcase=unittest.defaultTestLoader.discover(r'C:\Windows\System32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\apiAutotest\tests',pattern='test*.py')
     #testcase = unittest.defaultTestLoader.discover(test_dir,pattern='test*.py')
     #now = time.strftime("%Y-%m-%d_%H_%M_%S", time.localtime(time.time()))
-    filepath = r"E:\\{0}".format(os.path.basename(os.path.dirname(os.path.dirname(__file__))))+r"\\reports\\"
+    filepath = r"E:\\{0}".format(os.path.basename(os.path.dirname(os.path.dirname(__file__))))+r"\\reports\\{0}\\".format(fileName)
     if not os.path.exists(filepath):
         os.makedirs(filepath)
     ls = os.listdir(filepath)
     for i in ls:
         c_path = os.path.join(filepath, i)
         os.remove(c_path)
-    path = filepath + "report_{0}.html".format(fileName)
+    path = filepath + "{0}.html".format(fileName)
     fp = open(path, mode="wb")
     runner = HTMLTestReport.HTMLTestRunner(stream=fp, title="自动化测试报告", description="apiAutotest")
     runner.run(testcase)
