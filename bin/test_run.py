105a646bf7c39799ac33df53d69f1a6f8abbe8a0
ladywzf <2975724990@qq.com>
2021-10-15 12:15:02 +0800

    commit

--- bin/test_run.py
+++ bin/test_run.py
@@ -2,20 +2,21 @@ import pytest
 import unittest
 import os
 import HTMLTestReport
+from apiAutotest.tests.test_be import *
 import time
 if __name__ == '__main__':
     test_dir=os.path.join(os.path.dirname(os.path.dirname(__file__)),"tests")
     testcase=unittest.defaultTestLoader.discover(r'C:\Windows\System32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\apiAutotest\tests',pattern='test*.py')
     #testcase = unittest.defaultTestLoader.discover(test_dir,pattern='test*.py')
     #now = time.strftime("%Y-%m-%d_%H_%M_%S", time.localtime(time.time()))
-    filepath = r"E:\\{0}".format(os.path.basename(os.path.dirname(os.path.dirname(__file__))))+r"\\reports\\{1}\\".format(os.path.splitext(os.path.basename(os.path.abspath(__file__)))[0])
+    filepath = r"E:\\{0}".format(os.path.basename(os.path.dirname(os.path.dirname(__file__))))+r"\\reports\\"
     if not os.path.exists(filepath):
         os.makedirs(filepath)
     ls = os.listdir(filepath)
     for i in ls:
         c_path = os.path.join(filepath, i)
         os.remove(c_path)
-    path = filepath + "report.html"
+    path = filepath + "report_{0}.html".format(fileName)
     fp = open(path, mode="wb")
     runner = HTMLTestReport.HTMLTestRunner(stream=fp, title="自动化测试报告", description="apiAutotest")
     runner.run(testcase)
